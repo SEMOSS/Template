f7a78e5c70a1c0c6ff906fa9ec672ee79b7125a6
Rithvik Doshi <doshirithvik@gmail.com>
2025-10-20 15:29:08 -0400

    remove

--- .github/scripts/validate-semoss-compilation.py
+++ .github/scripts/validate-semoss-compilation.py
@@ -226,8 +226,6 @@ def upload_zip_file(server_connection, zip_filename, project_id):
             headers = {'Authorization': f'Basic {encoded}'}
             
             response = requests.post(upload_url, files=files, headers=headers, timeout=60)
-            print("the response should be here:")
-            print(response)
         
         response.raise_for_status()
         upload_result = response.json()
