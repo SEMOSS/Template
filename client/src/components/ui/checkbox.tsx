6fe6c320350fb649784850ef5704874eb73183d0
Van Buren <tevanburen@deloitte.com>
2025-12-02 09:41:19 -0500

    fix: shadcn

--- client/src/components/ui/checkbox.tsx
+++ client/src/components/ui/checkbox.tsx
@@ -1,6 +1,7 @@
+import * as React from "react";
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
 import { Check } from "lucide-react";
-import * as React from "react";
+
 import { cn } from "@/lib/utils";
 
 const Checkbox = React.forwardRef<
@@ -10,7 +11,7 @@ const Checkbox = React.forwardRef<
 	<CheckboxPrimitive.Root
 		ref={ref}
 		className={cn(
-			"peer grid h-4 w-4 shrink-0 place-content-center rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+			"grid place-content-center peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
 			className,
 		)}
 		{...props}
