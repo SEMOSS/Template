b4555bfa39589bbe4403bb937523625d4dbd4b0d
Van Buren <tevanburen@deloitte.com>
2026-01-28 14:30:00 -0500

    feat: allow no prop file

--- java/src/reactors/AbstractProjectReactor.java
+++ java/src/reactors/AbstractProjectReactor.java
@@ -95,7 +95,7 @@ public abstract class AbstractProjectReactor extends AbstractReactor {
       projectId = this.insight.getProjectId();
     }
 
-    // projectProperties = ProjectProperties.getInstance(projectId);
+    projectProperties = ProjectProperties.getInstance(projectId);
 
     // TODO: Initialize additional resources (engines, external services, etc.)
 
